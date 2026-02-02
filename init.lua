5f972538fd21bb34a67ea480388029fe888257f9
Matt-Kode <mtweaver04@yahoo.com>
2026-02-02 13:28:19 -0500

    init commit

---
+++ init.lua
@@ -0,0 +1,45 @@
+vim.g.mapleader = " "
+
+vim.cmd("set clipboard=unnamedplus")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+  spec = {
+    		{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    		{ 'nvim-telescope/telescope.nvim', version = '*', dependencies = {'nvim-lua/plenary.nvim',{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }}},
+    		{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' }
+	},
+		checker = { enabled = true },
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+	ensure_enabled = {'lua', 'javascript'},
+	highlight = { enable = true },
+	indent = { enable = true },
+})
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
