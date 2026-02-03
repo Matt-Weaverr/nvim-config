bf046adc278ed8cdcad9e6cab9e802acaa138ea7
Matt Kode <mtweaver04@yahoo.com>
2026-02-03 03:46:34 -0500

    tree-sitter finally

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,9 +3,37 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        require('nvim-treesitter').install({'lua', 'go', 'javascript'})
+        require('nvim-treesitter').install({'lua', 'go', 'javascript', 'php', 'python', 'c', 'html', 'css'})
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'lua' },
+            callback = function() vim.treesitter.start() end,
+        })
         vim.api.nvim_create_autocmd('FileType', {
             pattern = { 'go' },
+            callback = function() vim.treesitter.start() end,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'javascript' },
+            callback = function() vim.treesitter.start() end,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'php' },
+            callback = function() vim.treesitter.start() end,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'python' },
+            callback = function() vim.treesitter.start() end,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'c' },
+            callback = function() vim.treesitter.start() end,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'html' },
+            callback = function() vim.treesitter.start() end,
+        })
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'css' },
             callback = function() vim.treesitter.start() end,
         })
     end,
